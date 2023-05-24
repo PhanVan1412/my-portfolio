0750df9e458bde255c7e24e53876a459e4f9fbe9
unknown <phanvantam2020@gmail.com>
2023-05-24 21:10:01 +0700

    update about section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { motion } from 'framer-motion';
+import { Link } from 'react-router-dom';
 
 import { styles } from '../styles';
 import { ComputersCanvas } from './canvas';
@@ -22,7 +23,26 @@ const Hero = () => {
         </div>
        
       </div>
+
       <ComputersCanvas />
+
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+            <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+              <motion.div 
+                animate= {{
+                  y: [0, 24, 0]
+                }}
+                transition={{
+                  duration: 2,
+                  repeat: Infinity,
+                  repeatType: 'loop'
+                }}
+                className="w-3 h-3 rounded-full bg-secondary mb-1"
+              />
+            </div> 
+        </a>
+      </div> 
     </section>
   )
 }
--- src/constants/index.js
+++ src/constants/index.js
@@ -46,15 +46,15 @@ import {
       icon: web,
     },
     {
-      title: "React Native Developer",
+      title: "ReactJs Developer",
       icon: mobile,
     },
     {
-      title: "Backend Developer",
+      title: "AngularJS Developer",
       icon: backend,
     },
     {
-      title: "Content Creator",
+      title: "Wordpress Developer",
       icon: creator,
     },
   ];

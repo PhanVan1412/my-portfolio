ea9cfb45a051e78acfd445dd34d44ccb980e8cfa
unknown <phanvantam2020@gmail.com>
2023-05-31 07:26:34 +0700

    add section contact

--- src/components/canvas/index.js
+++ src/components/canvas/index.js
@@ -1,4 +1,6 @@
 import ComputersCanvas from "./Computers";
 import BallCanvas from "./Ball";
+import EarthCanvas from'./Earth';
+import StarsCanvas from "./Start";
 
-export { ComputersCanvas, BallCanvas };
+export { ComputersCanvas, BallCanvas, EarthCanvas, StarsCanvas };
